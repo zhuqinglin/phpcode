a7d100d8788d4e4771c5e3d6d1c505b1a7dab11b
朱庆林 <zhu.qinglin@immomo.com>
2021-03-01 14:51:42 +0800

    fix

--- arrayTopK.php
+++ arrayTopK.php
@@ -14,7 +14,7 @@
 
 /**
  * @param Integer[] $nums
- * @param Integer $k
+ * @param Integer   $k
  * @return Integer[]
  */
 function topKFrequent($nums, $k) {
@@ -22,15 +22,42 @@ function topKFrequent($nums, $k) {
     // 拼装成（值=>出现次数）的map
     $map = [];
     foreach ($nums as $item) {
-        $map[$item] = isset($map[$item]) ? $map[$item]++ : 1;
+        $map[$item] = isset($map[$item]) ? $map[$item] + 1 : 1;
+    }
+
+    $len = count($nums);
+    if ($k == $len) {
+        return array_keys($map);
+    }
+
+    $heap = new MyMaxHeap();
+
+    $res = [];
+    foreach ($map as $key => $value) {
+        if ($heap->count() < $len - $k) {
+            $heap->insert([$key, $value]);
+        } else if ($heap->top()[1] > $value) {
+            $res[] = $heap->extract()[0];
+            $heap->insert([$key, $value]);
+        } else {
+            $res[] = $key;
+        }
+    }
+
+    print_r($res);
+}
+
+$nums = [1];
+
+$k = 1;
+
+topKFrequent($nums, $k);
+
+class MyMaxHeap extends SplMaxHeap {
+    protected function compare($value1, $value2) {//自定义比较方法
+        return ($value1[1] - $value2[1]);
     }
 
 }
 
-
-class MyMinHeap extends SplMinHeap{
-    protected function  compare( $value1, $value2 ) {//自定义比较方法
-        return ( $value2[1] - $value1[1] );
-    }
-}
 
